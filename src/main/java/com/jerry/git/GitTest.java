50158423d9d1ba537b87bd3a253233ae15c825d5
Jerry-jy <1571849142@qq.com>
2022-05-17 21:44:42 +0800

    first commit

---
+++ src/main/java/com/jerry/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.jerry.git;
+
+/**
+ * @author Jerry
+ * @version 1.0.1
+ * @description
+ * @create 2022-05-17 21:41
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
